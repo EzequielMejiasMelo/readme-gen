d26e45d012ba0313ef3ef89066e88d5093974736
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-01-22 00:02:03 -0500

    Wrote license badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,36 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'No License (N/A)'){
+    return "";
+  }
+
+  let licenseName;
+  switch(license){
+    case 'MIT License':
+      licenseName = 'MIT';
+      break;
+    case 'GNU AGPLv3':
+      licenseName = 'AGPL_v3';
+      break;
+    case 'GNU GPLv3':
+      licenseName = 'GPLv3';
+      break;
+    case 'Mozilla Public License 2.0':
+      licenseName = 'MPL_2.0';
+      break;
+    case 'Apache License 2.0':
+      licenseName = 'Apache_2.0';
+      break;
+    case 'The Unlicense':
+      licenseName = 'Unlicense';
+      break;
+  }
+
+  let link = renderLicenseLink(license);
+  return `[![License: ${licenseName}](https://img.shields.io/badge/license-${licenseName}-blue.svg)](${link})`;
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
