c4b5a7ee0a1efdaaede5c91732c2165d023ec846
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-01-22 00:03:17 -0500

    Wrote license link function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -29,12 +29,39 @@ function renderLicenseBadge(license) {
 
   let link = renderLicenseLink(license);
   return `[![License: ${licenseName}](https://img.shields.io/badge/license-${licenseName}-blue.svg)](${link})`;
-
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "No License (N/A)"){
+    return "";
+  }
+
+  let licenseLink;
+  switch(license){
+    case 'MIT License':
+      licenseLink = "https://opensource.org/licenses/MIT";
+      break;
+    case 'GNU AGPLv3':
+      licenseLink = 'https://www.gnu.org/licenses/agpl-3.0';
+      break;
+    case 'GNU GPLv3':
+      licenseLink = 'https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    case 'Mozilla Public License 2.0':
+      licenseLink = 'https://opensource.org/licenses/MPL-2.0';
+      break;
+    case 'Apache License 2.0':
+      licenseLink = 'https://opensource.org/licenses/Apache-2.0';
+      break;
+    case 'The Unlicense':
+      licenseLink = 'http://unlicense.org/';
+      break;
+  }
+
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
